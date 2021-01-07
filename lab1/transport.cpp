ad9170227b16547e2b4b4f8e138e3b0d5feb12e2
ZERO <saschagame966@gmail.com>
2021-01-07 04:02:02 +0200

    perform practical work №3

---
+++ lab1/transport.cpp
@@ -0,0 +1,6 @@
+#include "transport.h"
+
+transport::transport()
+{
+
+}
---
+++ lab1/transport.h
@@ -0,0 +1,11 @@
+#ifndef TRANSPORT_H
+#define TRANSPORT_H
+
+
+class transport
+{
+public:
+    transport();
+};
+
+#endif // TRANSPORT_H
